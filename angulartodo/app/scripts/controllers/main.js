1776f2f6e169571e1adbe99665a1486093432281
Minh Duc Nguyen <dekarvn@gmail.com>
2013-08-23 16:01:15 +1000

    angular todo tutorial

---
+++ angulartodo/app/scripts/controllers/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('angulartodoApp')
+  .controller('MainCtrl', function ($scope, localStorageService) {
+	
+	// $scope.todos = ['Item1', 'Item2', 'Item3'];
+
+	var todosInStore = localStorageService.get('todos');
+	$scope.todos = todosInStore && todosInStore.split('\n') || [];
+
+	$scope.$watch(function(){
+		localStorageService.add('todos', $scope.todos.join('\n'));
+	});
+
+	$scope.addTodo = function(){
+		$scope.todos.push($scope.todo);
+		$scope.todo = '';
+	}
+
+	$scope.removeTodo = function(index){
+		$scope.todos.splice(index, 1);
+	}
+
+  });
