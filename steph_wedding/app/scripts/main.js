8a31a151131c909c6e055b15c98ae5fb15f0b807
Minh Duc Nguyen <dekarvn@gmail.com>
2013-08-24 19:15:40 +1000

    smooth scrolling, more content for steph_wedding

--- steph_wedding/app/scripts/main.js
+++ steph_wedding/app/scripts/main.js
@@ -1,1 +1,16 @@
-console.log('\'Allo \'Allo!');
+!function ($) {
+    $(function(){
+
+        var $root = $('html, body');
+
+        $('#nav a').click(function() {
+            var href = $.attr(this, 'href');
+            $root.animate({
+                scrollTop: $(href).offset().top-70
+            }, 500, function () {
+                window.location.hash = href;
+            });
+            return false;
+        });
+    })
+}(window.jQuery)
