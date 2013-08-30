df0d40e0bc7f53785ae6229c3d3445d71df7f8d3
Minh Duc Nguyen <dekarvn@gmail.com>
2013-08-30 20:44:22 +1000

    firebase integration / lots of changes

--- steph_wedding/app/scripts/main.js
+++ steph_wedding/app/scripts/main.js
@@ -4,6 +4,8 @@
         var $root = $('html, body');
 
         $('#nav a').click(function() {
+            $('#nav a').parent().removeClass('active');
+            $(this).parent().addClass('active');
             var href = $.attr(this, 'href');
             $root.animate({
                 scrollTop: $(href).offset().top
@@ -11,6 +13,10 @@
                 window.location.hash = href;
             });
             return false;
+        });
+
+        $(document).on('keydown', '#rsvp_form', function(e){
+            if(e.keyCode==13) e.preventDefault();
         });
     })
 }(window.jQuery)
