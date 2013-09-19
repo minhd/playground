29b330377f4de698cf45c6ba9d30c174800bbbd4
Minh Duc Nguyen <dekarvn@gmail.com>
2013-09-19 20:28:10 +1000

    update on algorithm for select best seat

--- sitting_plan/app/js/main.js
+++ sitting_plan/app/js/main.js
@@ -48,7 +48,8 @@ function mainCtrl($scope){
 					name:letter+''+i,
 					pos:i,
 					row:letter,
-					id:id
+					id:id,
+					row_ref:this_row
 				};
 				$scope.addSeat(this_row, seat);
 				$scope.all_seats.push(seat);
@@ -76,8 +77,36 @@ function mainCtrl($scope){
 		// row[seat.name] = {available:seat.available, id:seat.name};
 	}
 
+	$scope.closestAvailableSeat = function(seat){
+		var row = seat.row_ref;
+		var this_index = row.seats.indexOf(seat);
+		if(seat.available){
+			//check next seat
+			var next_seat = row.seats[this_index+1];
+			if(next_seat && next_seat.available == 'available'){
+				return next_seat;
+			}else{
+				var prev_seat = row.seats[this_index-1];
+				if(prev_seat && prev_seat.available == 'available'){
+					return prev_seat;
+				}
+			}
+			return false;
+		}
+		return false;
+	}
+
 	$scope.selectSeat = function(seat){
-		
+
+		// for(var i=0;i<$scope.group_size;i++){
+		// 	var closest = $scope.closestAvailableSeat(seat);
+		// 	if(closest){
+		// 		closest.available = 'selected';
+		// 		$scope.selected.push(closest);
+		// 	}
+		// }
+
+		$scope.remaining = $scope.group_size - $scope.selected.length;
 		if(seat.available=='available' && $scope.remaining > 0){
 			seat.available = 'selected';
 			$scope.selected.push(seat);
@@ -86,6 +115,10 @@ function mainCtrl($scope){
 			$scope.selected.pop(seat);
 		}else if($scope.remaining == 0){
 			//can't do anything, ran out of group size
+			$scope.selected[0].available='available';
+			$scope.selected.splice(0,1);
+			seat.available = 'selected';
+			$scope.selected.push(seat);
 		}
 		$scope.remaining = $scope.group_size - $scope.selected.length;
 	}
